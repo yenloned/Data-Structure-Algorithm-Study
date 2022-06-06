cae2b8323c8304ceeb87639496c312d0d2a9fc72
houch520 <57037777+houch520@users.noreply.github.com>
2022-06-06 15:32:11 +0800

    Update 128 Longest Consecutive Sequence.cpp

--- Array & Hashing/128 Longest Consecutive Sequence.cpp
+++ Array & Hashing/128 Longest Consecutive Sequence.cpp
@@ -1,15 +1,15 @@
 //Question: https://leetcode.com/problems/longest-consecutive-sequence/
-//Time Complexity O(nlog(n)+n-1)
+//Time Complexity: O(n)
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         if(nums.empty()){
             return 0;
         }
-        //sort() Time Complexity: O(N log(N))
+        //sort() Time Complexity: O(nlog(n))
         sort(nums.begin(),nums.end());
         int ans=1,temp =1;
-        //O(n-1)
+        //O(n)
         for(int i=0;i<nums.size()-1;i++){
             if(nums[i+1]-nums[i]==1){
                 temp++;
